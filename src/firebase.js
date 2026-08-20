6bff38fe76ede3bf37ec937848200050e3fb45e1
prathm7151 <prathmeshnaikbawne@gmail.com>
2026-08-20 22:03:40 +0530

    Update Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC-q9U585jAkKz0aCPw3gm87dpO5fnEhk8",
-    authDomain: "stock-analysis-1c842.firebaseapp.com",
-    projectId: "stock-analysis-1c842",
-    storageBucket: "stock-analysis-1c842.firebasestorage.app",
-    messagingSenderId: "632079957271",
-    appId: "1:632079957271:web:5523da2c7816d041174b52",
-    databaseURL: "https://stock-analysis-1c842-default-rtdb.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    databaseURL: ""
 };
 
 // Initialize Firebase
